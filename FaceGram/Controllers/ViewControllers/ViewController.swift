7fce7da99c937500d344900189bd0f95677ec420
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-07-17 17:11:01 -0600

    Trying to fix

--- FaceGram/Controllers/ViewControllers/ViewController.swift
+++ FaceGram/Controllers/ViewControllers/ViewController.swift
@@ -14,7 +14,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func threeInARow() {
+        
+        let firstArrayOfButtons = [button1, button2, button3]
+        let secondArrayOfButtons = [button4, button5, button6]
+        let thirdArrayOfButtons = [button7, button8, button9]
+        
+        var winningConditions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+        
+    }
 }
 
